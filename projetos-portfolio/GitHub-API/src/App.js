0c0f656528bcb432cf349112008e6a7621c8dac7
rlazani <rlazani@hotmail.com>
2022-07-20 21:43:30 -0300

    iniciando projeto React

---
+++ projetos-portfolio/GitHub-API/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Layout from "./components/layout";
+
+function App() {
+  return (
+    <main>
+      <Layout>
+        <div>User</div>
+        <div>Repositories</div>
+        <div>Stared</div>
+      </Layout>
+    </main>
+  );
+}
+
+export default App;
---
+++ projetos-portfolio/GitHub-API/src/components/layout/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Layout() {
+    return (
+        <section>
+            <header>
+                { children }
+            </header>
+        </section>
+    );
+}
+
+export default Layout
+
+
+// https://www.youtube.com/watch?v=F1HqLD6TgCw&list=PLTv2Rbwcr_Cru7KIHcffE1Shg9X9Eix7a&index=1
